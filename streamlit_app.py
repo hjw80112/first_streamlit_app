61c77449692e04fc644718989ef9aa8cddf3f30f
hjw80112 <138202210+hjw80112@users.noreply.github.com>
2023-07-02 21:42:27 +0100

    Update streamlit_app.py
    FIXED STOP()

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,8 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
